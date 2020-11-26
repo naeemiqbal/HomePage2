b5ccea322c5f39477974c4c60251a2c464bfc954
Naeem Iqbal <naeemiqbal@hotmail.com>
2020-11-26 16:48:24 -0500

    nmi431

--- src/main/java/com/aws/codestar/projecttemplates/configuration/MvcConfig.java
+++ src/main/java/com/aws/codestar/projecttemplates/configuration/MvcConfig.java
@@ -24,14 +24,11 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/").setCachePeriod(ONE_YEAR);
     }
 
-
-
     @Bean
     public InternalResourceViewResolver jspViewResolver() {
         InternalResourceViewResolver bean = new InternalResourceViewResolver();
-       // bean.setPrefix("/WEB-INF/views/");
+        bean.setPrefix("/WEB-INF/views/");
         bean.setSuffix(".jsp");
-
         return bean;
     }
 
